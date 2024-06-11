ed4612e637cd34c0ea8e3daa135920a66c3daeb8
gurok-yazilim-gelistirme <yazilim_gelistirme@gurok.com>
2024-06-11 12:00:54 +0300

    Dependency Inversion done with agnostic class

--- DependencyInversion/Program.cs
+++ DependencyInversion/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using DependencyInversion;
+
 Console.WriteLine("Hello, World!");
+/*
+ * Dependency Inversion Principle
+ * 
+ * Büyük sistemler (matkap) küçük sistemlere (uç) bağlı olmak yerine
+ * küçük sistemler büyük sistemlere aktarılmalıdır.
+ */
+
+ReportNotifier reportNotifier = new ReportNotifier();
+reportNotifier.sender = new MailSender();
+
+
---
+++ DependencyInversion/ReportNotifier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInversion
+{
+    internal class ReportNotifier // agnostic class
+    {
+        //public MailSender sender { get; set; }
+        private readonly ISender sender;
+
+        public ReportNotifier(ISender sender)
+        {
+            this.sender = sender;
+        }
+
+        public void SendReport()
+        {
+            //MailSender sender = new MailSender();
+            sender.Send();
+        }
+    }
+
+    public interface ISender
+    {
+        void Send();
+    }
+
+    public class MailSender: ISender
+    {
+        public void Send() => Console.WriteLine("Sending email...");
+    }
+
+    public class WhatsAppSender: ISender
+    {
+        public void Send() => Console.WriteLine("Sending WhatsApp message...");
+    }
+
+    public class TelegramSender: ISender
+    {
+        public void Send() => Console.WriteLine("Sending Telegram message...");
+    }
+}
